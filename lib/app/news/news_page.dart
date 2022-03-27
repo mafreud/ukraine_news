da06f2587726733ce8269f957a09d61a2c42d860
Masahiro Aoki <rafe.kun@gmail.com>
2022-03-27 20:39:51 +0900

    日時を正しく表示するように変更し、cardタップ時にリンク先を開くように変更

--- lib/app/news/news_page.dart
+++ lib/app/news/news_page.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ukraine_news/app/news/news_controller.dart';
+import 'package:ukraine_news/app/url_launcher/url_launcher_service.dart';
 
 class NewsPage extends ConsumerWidget {
   const NewsPage({Key? key}) : super(key: key);
@@ -13,24 +15,35 @@ class NewsPage extends ConsumerWidget {
         title: const Text('ニュース'),
       ),
       body: newsStream.when(
-          loading: () =>
-              const Center(child: CircularProgressIndicator.adaptive()),
-          error: (error, _) => Text(error.toString()),
-          data: (newsList) {
-            return ListView.builder(
-              itemCount: newsList.length,
-              itemBuilder: ((context, index) {
-                final news = newsList[index];
-                return Card(
+        loading: () =>
+            const Center(child: CircularProgressIndicator.adaptive()),
+        error: (error, _) => Text(error.toString()),
+        data: (newsList) {
+          return ListView.builder(
+            itemCount: newsList.length,
+            itemBuilder: ((context, index) {
+              final news = newsList[index];
+              return InkWell(
+                onTap: () => UrlLauncherService.launchURL(news.url),
+                child: Card(
                   child: ListTile(
                     leading: const Icon(Icons.south_america),
                     title: Text(news.title),
-                    subtitle: Text(news.publishedAt.toString()),
+                    subtitle: Text(
+                      timestampToDate(news.publishedAt),
+                    ),
                   ),
-                );
-              }),
-            );
-          }),
+                ),
+              );
+            }),
+          );
+        },
+      ),
     );
+  }
+
+  String timestampToDate(Timestamp timestamp) {
+    final date = timestamp.toDate();
+    return '${date.year}年${date.month}月${date.day}日';
   }
 }
